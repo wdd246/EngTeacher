25a0444c74f0ac0a897a6cb5213c8374d79e0f8d
SZtyro <SZtyro@users.noreply.github.com>
2019-04-27 09:08:47 +0200

    Text field modify

--- src/main/java/AddWordFrame.java
+++ src/main/java/AddWordFrame.java
@@ -2,12 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class AddWordFrame extends JFrame {
+    private JTextField enWordTextField,plWordTextField;
+
     public AddWordFrame(Rectangle parentFrameBounds){
         super("Dodaj słowo...");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(parentFrameBounds);
-        setLayout(new FlowLayout());
+        setLayout(new GridBagLayout());
         setVisible(true);
+
+        enWordTextField = new JTextField(20);
+        add(enWordTextField);
+
+
 
     }
 }
